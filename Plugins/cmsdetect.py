12b78cbec60c527b3cafdc11c47caeb0f962b081
Adithyan AK <adithyanhaxor@gmail.com>
2025-09-20 19:49:12 -0700

    Fix: 1 security vulnerabilities in cmsdetect.py

--- Plugins/cmsdetect.py
+++ Plugins/cmsdetect.py
@@ -1,7 +1,11 @@
+import os
 import requests
 
 def CMSdetect(domain, port):
-    payload = {'key': '1641c3b9f2b1c8676ceaba95d00f7cf2e3531830c5fa9a6cc5e2d922b2ed7165dcce66', 'url': domain}
+    api_key = os.getenv('WHATCMS_API_KEY')
+    if not api_key:
+        raise ValueError('API key for WhatCMS not found in environment variable WHATCMS_API_KEY')
+    payload = {'key': api_key, 'url': domain}
     cms_url = "https://whatcms.org/APIEndpoint/Detect"
     response = requests.get(cms_url, params=payload)
     cms_data = response.json()
